cbc40c7f3ef219c15ec58482f11b2a7f6a4d186b
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-02-07 11:14:48 +0100

    Add och multiply

--- src/kompltal.java
+++ src/kompltal.java
@@ -2,5 +2,21 @@ public class kompltal {
     double a;
     double b;
 
+    public kompltal(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+    public kompltal add(kompltal z2) {
+        double a = this.a + z2.a;
+        double b = this.b + z2.b;
+        kompltal z3 = new kompltal(a,b);
+        return z3;
+    }
+    public kompltal multiply(kompltal z2) {
+        double a = this.a * z2.a;
+        double b = this.a * z2.b;
+        kompltal z4 = new kompltal(a,b);
+        return z4;
+    }
     
 }
--- src/testKompltal.java
+++ src/testKompltal.java
@@ -1,3 +1,13 @@
 public class testKompltal {
+    public static void main(String[] args) {
+        kompltal z1 = new kompltal(2, 3);
+        kompltal z2 = new kompltal(4, -2);
 
+        kompltal z3 = z1.add(z2);
+        kompltal z4 = z1.multiply(z2);
+
+        System.out.println(z3);
+        System.out.println(z4);
+
+    }
 }
